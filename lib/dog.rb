4e3f095591aecef770a65ba73535d7889b832c8f
mcgillshakore <mcgills2093@gmail.com>
2020-06-04 12:46:03 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def name
+        @name
+    end
+    def name=(new_value)
+        @name = new_value
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(new_value)
+        @breed = new_value
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name
+        @name
+    end
+    def name=(new_value)
+        @name = new_value
+    end
+
+    def job
+        @job
+    end
+
+    def job=(new_value)
+        @job = new_value
+    end
+    
+end
